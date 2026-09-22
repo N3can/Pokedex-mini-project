ce5b553249376d95b8064acc14fc1ba5a51e6e69
Necan <167892971+N3can@users.noreply.github.com>
2026-09-22 17:37:56 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/pokedex-mini/', // replace with your own repo name
+  base: '/Pokedex-mini-project/', // matches your actual GitHub repo name exactly, case-sensitive
   plugins: [react()],
 })
